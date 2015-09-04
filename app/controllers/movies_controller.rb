08e82de3503376d5a206197f6cc453c740b38783
dananagle <dananagle@Danas-Air.attlocal.net>
2015-09-04 04:23:14 -0500

    Fixed sorting, changed some styling, Fixed redirect from create.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,18 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+#  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def dashboard
+  end
+
+  #  def show
+  #  end
 
   # GET /movies
 
   def index
     @movies = Movie.all
-  end
-
-  # GET /movies/1
-
-  def show
+    @sort_column = sort_column
+    @movies = Movie.order(@sort_column.order)
   end
 
   # GET /movies/new
@@ -18,57 +21,82 @@ class MoviesController < ApplicationController
   end
 
   # GET /movies/1/edit
-  def edit
-  end
+#  def edit
+#  end
 
   # POST /movies
 
   def create
-    @movie = Movie.new(movie_params)
+#    respond_to do |format|
+#      if @movie.save
+#        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+#      else
+#        format.html { render :new }
+#      end
+#    end
+  #end
+    @movie = Movie.new params.require(:movie).permit(:title, :release, :format, :length, :rating)
+    @movie.save!
+    redirect_to index_path
+  end
 
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
+  def show
+    @movie = Movie.find params[:id]
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
   end
 
   # PATCH/PUT /movies/1
 
   def update
-    respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: 'Update Successful.' }
-        format.json { render :show, status: :ok, location: @movie }
+#    respond_to do |format|
+      @movie = Movie.find params[:id]
+      if @movie.update params.require(:movie).permit(:title, :release, :format,  :length, :rating)
+        redirect_to index_path
       else
-        format.html { render :edit }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
+        render :edit
     end
   end
+
+  def sort_column
+   title_sort = SortableTable::SortColumnCustomDefinition.new('title',
+     asc: 'title asc',
+     desc: 'title desc')
+
+   release_sort = SortableTable::SortColumnDefinition.new('release')
+
+   format_sort = SortableTable::SortColumnDefinition.new('format')
+
+   length_sort = SortableTable::SortColumnDefinition.new('length')
+
+   rating_sort = SortableTable::SortColumnDefinition.new('rating')
+
+   sort_table = SortableTable::SortTable.new([title_sort, release_sort, format_sort, length_sort, rating_sort])
+   sort_table.sort_column(params[:sort], params[:direction])
+ end
 
   # DELETE /movies/1
 
   def destroy
+    @movie = Movie.find(params[:id])
     @movie.destroy
-    respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+#    respond_to do |format|
+#      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+#      format.json { head :no_content }
+#    end
   end
 
-  private
+#  private
     # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
+#    def set_movie
+#      @movie = Movie.find(params[:id])
+#    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def movie_params
-      params.require(:movie).permit(:title, :format, :length, :release, :rating)
-    end
+#    def movie_params
+#      params.require(:movie).permit(:title, :format, :length, :release, :rating)
+#    end
 end
